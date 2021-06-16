dddc9651fa7f513727104a1da8b64fff5ccf2c26
rathore287 <harshraj.rathore@gmail.com>
2021-06-16 13:54:17 +0100

    database

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from core.config import settings
-import os
+from db.session import engine
+from db.base_url import Base
 
-app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+
+def start_app():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    create_table()
+    return app
+
+def create_table():
+    Base.metadata.create_all(bind=engine)
+
+app=start_app()
 
 @app.get('/')
 def hello_api():
